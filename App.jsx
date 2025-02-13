792ca745620b80986adc6607711b2d241b4729b9
lessaless <gabriellessa285@gmail.com>
2025-02-13 04:54:54 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+// Iniciar com npm run dev
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+  <>
+  <Header /> 
+  <Main/>
+  
+  </>
+
+  );
+};
+
+export default App;
