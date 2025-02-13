72cdb8774d0a3d3767e49cfb87d540a6fdb66ed0
lessaless <gabriellessa285@gmail.com>
2025-02-13 04:53:53 -0300

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logospotify from "../assets/logo/spotify-logo.png";
+
+const Header = () => {
+  return (
+  <div className="header">
+    <img src={logospotify} alt="Logo Spotify"/> {/* coloco entre chaves pq é jsx. se fosse html , nao teria esse problema */}
+      
+    <a className="header__link" href="/">
+        <h1>Spotify</h1>
+    </a>
+  </div>
+    
+  );
+  
+};
+
+export default Header;
